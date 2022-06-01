3a63ff26b58643081a5b4dd60ea13acfabdbb342
Farhan <faarhan7@gmail.com>
2022-06-01 23:03:22 +0700

    Components Added

---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <h1>{{title}}</h1>
+    <Button />
+  </header>
+</template>
+
+<script>
+import { Options, Vue } from 'vue-class-component';
+import Button from './Button.vue';
+
+export default{
+  name: 'Header',
+  props:{
+    title: String,
+  },
+  components: {
+    Button,
+  }
+}
+// @Options({
+//   props:{
+//     title: String
+//   },
+//   components: {
+//     Button
+//   }
+// })
+// export default class Header extends Vue {}
+</script>
+
+<style scoped>
+  header{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 20px;
+  }
+</style>
